85333a58be0b7e1e98997cb41bfb81a173f8eb7a
mmk <mmk@rock-chips.com>
2015-04-30 17:53:39 +0800

    add android_5.1 drm L3 for sofia

--- widevine.mk
+++ widevine.mk
@@ -4,11 +4,22 @@ CUR_PATH := vendor/widevine
 ########################################################
 #add drm function
 #######################################################
-PRODUCT_COPY_FILES += vendor/widevine/libs/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
-                     vendor/widevine/libs/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
-                     vendor/widevine/libs/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
-                     vendor/widevine/libs/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
-                     vendor/widevine/libs/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
-                     vendor/widevine/libs/libwvm.so:system/vendor/lib/libwvm.so \
-                     vendor/widevine/framework/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar \
-                     vendor/widevine/framework/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml
+ifeq ($(strip $(TARGET_BOARD_HARDWARE)), sofiaboard)
+PRODUCT_COPY_FILES += vendor/widevine/libs/x86/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
+                     vendor/widevine/libs/x86/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
+                     vendor/widevine/libs/x86/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+                     vendor/widevine/libs/x86/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
+                     vendor/widevine/libs/x86/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
+                     vendor/widevine/libs/x86/libwvm.so:system/vendor/lib/libwvm.so \
+                     vendor/widevine/framework/x86/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar \
+                     vendor/widevine/framework/x86/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml
+else
+PRODUCT_COPY_FILES += vendor/widevine/libs/arm/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
+                     vendor/widevine/libs/arm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
+                     vendor/widevine/libs/arm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
+                     vendor/widevine/libs/arm/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
+                     vendor/widevine/libs/arm/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
+                     vendor/widevine/libs/arm/libwvm.so:system/vendor/lib/libwvm.so \
+                     vendor/widevine/framework/arm/com.google.widevine.software.drm.jar:system/framework/com.google.widevine.software.drm.jar \
+                     vendor/widevine/framework/arm/com.google.widevine.software.drm.xml:system/etc/permissions/com.google.widevine.software.drm.xml
+endif
